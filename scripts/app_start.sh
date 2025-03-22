ad15cd422623f006efb795ea695ad9c9583736f3
Yembuluru Rajaneesh <101640141+Rajaneesh2307@users.noreply.github.com>
2025-03-22 14:19:14 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -2,24 +2,16 @@
 echo "🚀 Running ApplicationStart script for React Vite app..."
 
 echo "⚡ Installing serve..."
-npm install -g serve || {
-    echo "❌ Failed to install serve! Exiting."
-    exit 1
-}
+npm install -g serve
 
 echo "⚡ Installing PM2..."
-npm install -g pm2 || {
-    echo "❌ Failed to install PM2! Exiting."
-    exit 1
-}
+sudo npm install -g pm2
 
 echo "🛑 Stopping existing Vite app (if running)..."
-pm2 stop all || echo "⚠️ No existing PM2 process found to stop."
+/usr/local/bin/pm2 stop all || echo "⚠️ No existing PM2 process found to stop."
 
 echo "🚀 Starting Vite app..."
-pm2 start "serve -s dist" --name "vite-app" || {
+/usr/local/bin/pm2 start "serve -s dist -l 3000" --name react-vite-app || {
     echo "❌ Failed to start Vite app with PM2! Exiting."
     exit 1
 }
-
-echo "✅ Application started successfully!"
